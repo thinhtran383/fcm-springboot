c2fd69d446867b647eb53184cf0470f6b712a30e
ThinhTran <thinhtran3803@gmail.com>
2024-03-10 23:52:40 +0700

    feat: send notification fcm

--- src/main/java/com/example/fcmspringboot/FcmSpringBootApplication.java
+++ src/main/java/com/example/fcmspringboot/FcmSpringBootApplication.java
@@ -1,10 +1,32 @@
 package com.example.fcmspringboot;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class FcmSpringBootApplication {
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(
+                new ClassPathResource("food-ordering-1666f-firebase-adminsdk-pel1z-cba53403f1.json").getInputStream()
+        );
+
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(googleCredentials).build();
+
+        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+        return FirebaseMessaging.getInstance(app);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(FcmSpringBootApplication.class, args);
---
+++ src/main/java/com/example/fcmspringboot/NotificationMessage.java
@@ -0,0 +1,14 @@
+package com.example.fcmspringboot;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class NotificationMessage {
+    private String recipientToken;
+    private String title;
+    private String body;
+    private String image;
+    private Map<String, String> data;
+}
