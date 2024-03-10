10885113fcaa2d5331a66b16c64a832129ab88df
ThinhTran <thinhtran3803@gmail.com>
2024-03-10 22:08:08 +0700

    init

---
+++ src/main/java/com/example/fcmspringboot/FcmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcmspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fcmspringboot/FcmSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcmspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
